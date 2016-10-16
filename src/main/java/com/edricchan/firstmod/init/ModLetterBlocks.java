49ab34f6931803a464c7d33880f38c34472b0167
Edric Chan <edric.chan.1997@gmail.com>
2016-10-16 21:26:52 +0800

    Forgot to add blocks!

--- src/main/java/com/edricchan/firstmod/init/ModLetterBlocks.java
+++ src/main/java/com/edricchan/firstmod/init/ModLetterBlocks.java
@@ -8,7 +8,24 @@ import com.edricchan.firstmod.letter_blocks.LetterEOrange;
 import com.edricchan.firstmod.letter_blocks.LetterFOrange;
 import com.edricchan.firstmod.letter_blocks.LetterGOrange;
 import com.edricchan.firstmod.letter_blocks.LetterHOrange;
+import com.edricchan.firstmod.letter_blocks.LetterIOrange;
+import com.edricchan.firstmod.letter_blocks.LetterJOrange;
+import com.edricchan.firstmod.letter_blocks.LetterKOrange;
+import com.edricchan.firstmod.letter_blocks.LetterLOrange;
+import com.edricchan.firstmod.letter_blocks.LetterMOrange;
+import com.edricchan.firstmod.letter_blocks.LetterNOrange;
+import com.edricchan.firstmod.letter_blocks.LetterOOrange;
+import com.edricchan.firstmod.letter_blocks.LetterPOrange;
+import com.edricchan.firstmod.letter_blocks.LetterQOrange;
+import com.edricchan.firstmod.letter_blocks.LetterROrange;
+import com.edricchan.firstmod.letter_blocks.LetterSOrange;
 import com.edricchan.firstmod.letter_blocks.LetterTOrange;
+import com.edricchan.firstmod.letter_blocks.LetterUOrange;
+import com.edricchan.firstmod.letter_blocks.LetterVOrange;
+import com.edricchan.firstmod.letter_blocks.LetterWOrange;
+import com.edricchan.firstmod.letter_blocks.LetterXOrange;
+import com.edricchan.firstmod.letter_blocks.LetterYOrange;
+import com.edricchan.firstmod.letter_blocks.LetterZOrange;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
@@ -27,7 +44,24 @@ public class ModLetterBlocks {
 	public static Block letter_f_orange;
 	public static Block letter_g_orange;
 	public static Block letter_h_orange;
+	public static Block letter_i_orange;
+	public static Block letter_j_orange;
+	public static Block letter_k_orange;
+	public static Block letter_l_orange;
+	public static Block letter_m_orange;
+	public static Block letter_n_orange;
+	public static Block letter_o_orange;
+	public static Block letter_p_orange;
+	public static Block letter_q_orange;
+	public static Block letter_r_orange;
+	public static Block letter_s_orange;
 	public static Block letter_t_orange;
+	public static Block letter_u_orange;
+	public static Block letter_v_orange;
+	public static Block letter_w_orange;
+	public static Block letter_x_orange;
+	public static Block letter_y_orange;
+	public static Block letter_z_orange;
 	
 	// Default colours
 	public static Block letter_orange;
@@ -42,7 +76,24 @@ public class ModLetterBlocks {
 		letter_f_orange = new LetterFOrange();
 		letter_g_orange = new LetterGOrange();
 		letter_h_orange = new LetterHOrange();
+		letter_i_orange = new LetterIOrange();
+		letter_j_orange = new LetterJOrange();
+		letter_k_orange = new LetterKOrange();
+		letter_l_orange = new LetterLOrange();
+		letter_m_orange = new LetterMOrange();
+		letter_n_orange = new LetterNOrange();
+		letter_o_orange = new LetterOOrange();
+		letter_p_orange = new LetterPOrange();
+		letter_q_orange = new LetterQOrange();
+		letter_r_orange = new LetterROrange();
+		letter_s_orange = new LetterSOrange();
 		letter_t_orange = new LetterTOrange();
+		letter_u_orange = new LetterUOrange();
+		letter_v_orange = new LetterVOrange();
+		letter_w_orange = new LetterWOrange();
+		letter_x_orange = new LetterXOrange();
+		letter_y_orange = new LetterYOrange();
+		letter_z_orange = new LetterZOrange();
 	}
 	
 	public static void register() {
@@ -54,7 +105,24 @@ public class ModLetterBlocks {
 		registerBlock(letter_f_orange);
 		registerBlock(letter_g_orange);
 		registerBlock(letter_h_orange);
+		registerBlock(letter_i_orange);
+		registerBlock(letter_j_orange);
+		registerBlock(letter_k_orange);
+		registerBlock(letter_l_orange);
+		registerBlock(letter_m_orange);
+		registerBlock(letter_n_orange);
+		registerBlock(letter_o_orange);
+		registerBlock(letter_p_orange);
+		registerBlock(letter_q_orange);
+		registerBlock(letter_r_orange);
+		registerBlock(letter_s_orange);
 		registerBlock(letter_t_orange);
+		registerBlock(letter_u_orange);
+		registerBlock(letter_v_orange);
+		registerBlock(letter_w_orange);
+		registerBlock(letter_x_orange);
+		registerBlock(letter_y_orange);
+		registerBlock(letter_z_orange);
 	}
 	
 	private static void registerBlock(Block block) {
@@ -73,7 +141,24 @@ public class ModLetterBlocks {
 		registerRender(letter_f_orange);
 		registerRender(letter_g_orange);
 		registerRender(letter_h_orange);
+		registerRender(letter_i_orange);
+		registerRender(letter_j_orange);
+		registerRender(letter_k_orange);
+		registerRender(letter_l_orange);
+		registerRender(letter_m_orange);
+		registerRender(letter_n_orange);
+		registerRender(letter_o_orange);
+		registerRender(letter_p_orange);
+		registerRender(letter_q_orange);
+		registerRender(letter_r_orange);
+		registerRender(letter_s_orange);
 		registerRender(letter_t_orange);
+		registerRender(letter_u_orange);
+		registerRender(letter_v_orange);
+		registerRender(letter_w_orange);
+		registerRender(letter_x_orange);
+		registerRender(letter_y_orange);
+		registerRender(letter_z_orange);
 		
 		// Default colours
 		registerBlock(letter_orange);
