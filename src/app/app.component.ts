2d02e7e401e224fdcb38dae4b0e7bcf8a57e6129
Edric Chan <edric.chan.1997@gmail.com>
2017-10-28 14:58:37 +0800

    fix: Remove placeholders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Shared } from './shared';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+import * as hljs from 'highlight.js';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+	constructor(private breakpointObserver: BreakpointObserver, private shared: Shared){}
+	@ViewChild('left') sidenav: MatSidenav;
+	get isSidenavOpen() {
+		if (this.sidenav.opened) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	get isMobile() {
+		if (this.breakpointObserver.isMatched('(max-width: 599px)')) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	links: NavLink[] = [
+		{
+			link: "home",
+			title: "Home",
+			icon: "home"
+		},
+		{
+			link: "downloads",
+			title: "Downloads",
+			icon: "file_download"
+		},
+		{
+			link: "guides",
+			title: "Guides"
+		}
+	];
+	ngOnInit() {
+		hljs.initHighlightingOnLoad();
+	}
+}
+
+export interface NavLink {
+	link: string;
+	title: string;
+	icon?: string;
 }
