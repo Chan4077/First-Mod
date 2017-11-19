98fc88622fc29fe172884e0d6cfa97415bd07dc6
Edric Chan <edric.chan.1997@gmail.com>
2017-11-19 16:17:11 +0800

    Add docs items

---
+++ src/app/docs/docs-items.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+const GUIDE_ITEMS: DocItem[] = [
+	{
+		name: "Building the Source Code",
+		id: "building",
+		url: "guides/building"
+	},
+	{
+		name: "Getting Started",
+		id: "getting-started",
+		url: "guides/getting-started"
+	},
+	{
+		name: "Community",
+		id: "community",
+		url: "guides/community"
+	}
+];
+
+@Injectable()
+export class DocsItems {
+	getDocItems(): DocItem[] {
+		return GUIDE_ITEMS;
+	}
+	getDocItemById(id: string): DocItem {
+		return GUIDE_ITEMS.find(i => i.id === id);
+	}
+}
+
+export interface DocItem {
+	name: string;
+	id: string;
+	url: string;
+}
