e6e974b6ee328ddcc8b4f08c73989851e2085301
Edric Chan <edric.chan.1997@gmail.com>
2017-11-19 16:19:47 +0800

    chore: Add index file

---
+++ src/app/docs/index.ts
@@ -0,0 +1,5 @@
+export * from './building/building.component';
+export * from './community/community.component';
+export * from './getting-started/getting-started.component';
+export * from './guide-home/guide-home.component';
+export * from './docs-items';
